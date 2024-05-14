6deff073c2c72400772ed7d0a640a8999dc6e2d6
shafadisya <163166271+shafadisya@users.noreply.github.com>
2024-05-14 13:56:46 +0700

    Add files via upload

---
+++ mengurangi_brg.c
@@ -0,0 +1,22 @@
+#include "header.h"
+
+// Fungsi untuk mengurangi jumlah barang
+void kurangiBarang(Barang daftarBarang[], int jumlahBarang) {
+    tampilkanBarang(daftarBarang, jumlahBarang);
+
+    int pilihan, jumlahKurang;
+    printf("Pilih barang yang ingin dikurangi jumlahnya (nomor): ");
+    scanf("%d", &pilihan);
+    if (pilihan >= 1 && pilihan <= jumlahBarang) {
+        printf("Masukkan jumlah barang yang ingin dikurangi: ");
+        scanf("%d", &jumlahKurang);
+        if (jumlahKurang > 0 && jumlahKurang <= daftarBarang[pilihan-1].jumlah) {
+            daftarBarang[pilihan-1].jumlah -= jumlahKurang;
+            printf("Jumlah barang berhasil dikurangi.\n");
+        } else {
+            printf("Jumlah yang dimasukkan tidak valid.\n");
+        }
+    } else {
+        printf("Nomor barang tidak valid.\n");
+    }
+}
