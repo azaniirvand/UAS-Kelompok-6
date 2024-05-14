b804a61ef63efe206282488b7453f06fe168cbaf
azaniirvand <163728366+azaniirvand@users.noreply.github.com>
2024-05-14 14:19:55 +0700

    Add files via upload

---
+++ login.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+// Fungsi untuk login
+int login(Login loginInfo[], int *jumlahLogin) {
+    char username[50], password[50];
+    printf("Masukkan username: ");
+    scanf(" %s", username);
+    printf("Masukkan password: ");
+    scanf("%s", password);
+    for (int i = 0; i < *jumlahLogin; i++) {
+        if (strcmp(username, loginInfo[i].username) == 0 && strcmp(password, loginInfo[i].password) == 0)
+            return 1;
+    }
+    return 0;
+}
