a34fba17624dc1d556ec1fac8747cb9e349fbefa
shafadisya <163166271+shafadisya@users.noreply.github.com>
2024-05-14 14:16:43 +0700

    Update total_bayar.c

--- total_bayar.c
+++ total_bayar.c
@@ -1,5 +1,5 @@
 // Fungsi untuk menghitung total pembayaran
-double hitungTotalPembayaran(Barang daftarPembelian[], int jumlahPembelian) {
+double MenghitungTotalPembayaran(Barang daftarPembelian[], int jumlahPembelian) {
     double total = 0;
     for (int i = 0; i < jumlahPembelian; i++) {
         total += daftarPembelian[i].jumlah * daftarPembelian[i].harga;
