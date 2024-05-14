9e5454d108401df272602f6120bc9cfbd77a9563
Razian Sabri <142809901+friderv@users.noreply.github.com>
2024-05-14 14:06:58 +0700

    Add files via upload

---
+++ catat_beli.c
@@ -0,0 +1,16 @@
+// Fungsi untuk mencatat pembelian
+void catatPembelian(Barang daftarPembelian[], int jumlahPembelian) {
+    FILE *pembelianFile = fopen("pembelian.txt", "a");
+    if (pembelianFile == NULL) {
+        printf("Gagal membuka file pembelian.txt.\n");
+        return;
+    }
+
+    fprintf(pembelianFile, "Pembelian baru:\n");
+    for (int i = 0; i < jumlahPembelian; i++) {
+        fprintf(pembelianFile, "%s - Jumlah: %d - Harga: %.2f\n", daftarPembelian[i].nama, daftarPembelian[i].jumlah, daftarPembelian[i].harga);
+    }
+    fprintf(pembelianFile, "\n");
+
+    fclose(pembelianFile);
+}
