19bde017184ca7f30f1156c4739069fe1d9999f9
azaniirvand <163728366+azaniirvand@users.noreply.github.com>
2024-05-14 14:21:47 +0700

    Add files via upload

---
+++ MenampilkanBarang.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+// Fungsi untuk menampilkan daftar barang
+void tampilkanBarang(Barang daftarBarang[], int jumlahBarang) {
+    printf("Daftar Barang:\n");
+    for (int i = 0; i < jumlahBarang; i++) {
+        printf("%d. %s - Jumlah: %d - Harga: %.2f\n", i+1, daftarBarang[i].nama, daftarBarang[i].jumlah, daftarBarang[i].harga);
+    }
+}
